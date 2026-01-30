09bb62c1050114b625767c42281f1cc8dc029ca6
yangjunsig <wnstlr1379@naver.com>
2026-01-30 21:26:01 +0900

    first commit

---
+++ src/main/java/com/blaybus/blaybusbe/BlaybusBeApplication.java
@@ -0,0 +1,13 @@
+package com.blaybus.blaybusbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlaybusBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlaybusBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/blaybus/blaybusbe/BlaybusBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blaybus.blaybusbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlaybusBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
